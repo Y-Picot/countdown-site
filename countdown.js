92885e0393fc554ba45e2fae313e0d4a51cad20c
ybeater <ytdevelopement@hotmail.com>
2023-04-27 11:28:33 +0200

    #1 Partie responsive

--- countdown.js
+++ countdown.js
@@ -6,22 +6,22 @@ var x = setInterval(function() {
 
   // Get today's date and time
   var now = new Date().getTime();
-  
+
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-  
+
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
+
   // Display the result in the element with class="countdown-timer"
   document.querySelector(".days").innerHTML = days;
   document.querySelector(".hours").innerHTML = hours;
   document.querySelector(".minutes").innerHTML = minutes;
   document.querySelector(".seconds").innerHTML = seconds;
-  
+
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
