dbf00e2c40edebf7eb42f9cdfb013755a29933e7
ybeater <ytdevelopement@hotmail.com>
2023-07-26 12:06:28 +0200

    #10 Comformite SEO

--- countdown.js
+++ countdown.js
@@ -26,7 +26,7 @@ var x = setInterval(function() {
   }
 }, 1000);
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 21, 2023 15:00:00").getTime();
+var countDownDate = new Date("Jul 15, 2023 19:45:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
